1554a539e031acc4890a07240c568c707b16be35
Aqua200 <chinquepapa@gmail.com>
2025-01-29 22:09:27 -0800

    Update _Buttons.js

--- plugins/_Buttons.js
+++ plugins/_Buttons.js
@@ -1,14 +1,14 @@
 import { proto, generateWAMessage, areJidsSameUser } from '@whiskeysockets/baileys';
 
-async function before(m, chatUpdate) {
+async function before(m, chatUpdate, conn) { // Añadido conn como argumento
     if (m.isBaileys) return;
     if (!m.message) return;
 
     if (m.mtype === "interactiveResponseMessage" && m.quoted.fromMe) {
-        appendTextMessage(JSON.parse(m.msg.nativeFlowResponseMessage.paramsJson).id, chatUpdate);
+        appendTextMessage(JSON.parse(m.msg.nativeFlowResponseMessage.paramsJson).id, chatUpdate, conn); // Añadido conn como argumento
     }
     
-    async function appendTextMessage(text, chatUpdate) {
+    async function appendTextMessage(text, chatUpdate, conn) { // Añadido conn como argumento
         try {
             let messages = await generateWAMessage(m.chat, { text: text, mentions: m.mentionedJid }, {
                 userJid: conn.user.id,
