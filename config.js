2020819a3f03af615596019555b316cb9b39ab9e
Aqua200 <chinquepapa@gmail.com>
2025-01-29 17:24:58 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,67 +1,83 @@
-import { watchFile, unwatchFile } from 'fs' 
-import chalk from 'chalk'
-import { fileURLToPath } from 'url'
-import fs from 'fs'
-import cheerio from 'cheerio'
-import fetch from 'node-fetch'
-import axios from 'axios'
-import moment from 'moment-timezone' 
+import { watchFile, unwatchFile } from 'fs';
+import chalk from 'chalk';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import moment from 'moment-timezone';
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+//*─────────────────────────────────────────────────────────────────*
 
 global.owner = [
   ['', 'Neykoor', true],
   ['', '', true],
-]
+];
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+//*─────────────────────────────────────────────────────────────────*
 
-global.mods = []
-global.prems = []
-   
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.mods = [];
+global.prems = [];
 
-global.packname = `⛩️Anika✨`
-global.author = '⛩️Anika✨'
-global.wait = '⛩️Anika✨'
-global.botname = '⛩️Anika✨'
-global.textbot = `⛩️Anika para servirle✨`
-global.listo = '⛩️Anika todo listo✨'
-global.namechannel = '⛩️Anika✨'
-global.baileys = '@whiskeysockets/baileys'
+//*─────────────────────────────────────────────────────────────────*
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.packname = '⛩️Anika✨';
+global.author = '⛩️Anika✨';
+global.wait = '⛩️Anika✨';
+global.botname = '⛩️Anika✨';
+global.textbot = '⛩️Anika para servirle✨';
+global.listo = '⛩️Anika todo listo✨';
+global.namechannel = '⛩️Anika✨';
+global.baileys = '@whiskeysockets/baileys';
 
-global.catalogo = fs.readFileSync('./storage/img/catalogo.png')
-global.siskedurl = fs.readFileSync('./storage/img/siskedurl.jpg')
+//*─────────────────────────────────────────────────────────────────*
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.catalogo = fs.readFileSync('./storage/img/catalogo.png');
+global.siskedurl = fs.readFileSync('./storage/img/siskedurl.jpg');
 
-global.group = 'https://chat.whatsapp.com/KGdftYYFrbVBzTpZE73LBI'
-global.canal = 'https://whatsapp.com/channel/0029Vb3uTsb90x2rvI6D3G3b'
+//*─────────────────────────────────────────────────────────────────*
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.group = 'https://chat.whatsapp.com/KGdftYYFrbVBzTpZE73LBI';
+global.canal = 'https://whatsapp.com/channel/0029Vb3uTsb90x2rvI6D3G3b';
 
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: botname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+//*─────────────────────────────────────────────────────────────────*
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.estilo = { 
+  key: { 
+    fromMe: false, 
+    participant: '0@s.whatsapp.net', 
+    ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) 
+  }, 
+  message: { 
+    orderMessage: { 
+      itemCount: -999999, 
+      status: 1, 
+      surface: 'CATALOG', 
+      message: 'Anika Bot', 
+      orderTitle: 'Menu',
+      sellerJid: '0@s.whatsapp.net' 
+    } 
+  } 
+};
 
-global.cheerio = cheerio
-global.fs = fs
-global.fetch = fetch
-global.axios = axios
-global.moment = moment	
+//*─────────────────────────────────────────────────────────────────*
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.cheerio = cheerio;
+global.fs = fs;
+global.fetch = fetch;
+global.axios = axios;
+global.moment = moment;
 
-global.multiplier = 69 
-global.maxwarn = '2' // máxima advertencias
+//*─────────────────────────────────────────────────────────────────*
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.multiplier = 69;
+global.maxwarn = '2'; // máxima advertencias
 
-let file = fileURLToPath(import.meta.url)
+//*─────────────────────────────────────────────────────────────────*
+
+let file = fileURLToPath(import.meta.url);
 watchFile(file, () => {
-  unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
-  import(`${file}?update=${Date.now()}`)
-})
+  unwatchFile(file);
+  console.log(chalk.redBright("Update 'config.js'"));
+  import(`${file}?update=${Date.now()}`);
+});
