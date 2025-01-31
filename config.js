61b48bfe4080720382f31105df4578d99a9a626c
Aqua200 <chinquepapa@gmail.com>
2025-01-31 02:08:48 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,86 +1,39 @@
-#!/usr/bin/env node
-
-import { watchFile, unwatchFile } from 'fs';
-import chalk from 'chalk';
-import { fileURLToPath } from 'url';
-import fs from 'fs';
-import * as cheerio from 
-  'cheerio';
-import fetch from 'node-fetch';
-import axios from 'axios';
-import moment from 'moment-timezone';
-
-//*─────────────────────────────────────────────────────────────────*
-
-global.owner = [
-  ['', 'Neykoor', true],
-  ['', '', true],
-];
-
-//*─────────────────────────────────────────────────────────────────*
-
-global.mods = [];
-global.prems = [];
-
-//*─────────────────────────────────────────────────────────────────*
-
-global.packname = '🌸Anika✧🌙';
-global.author = '🌸Anika✧🌙';
-global.wait = '🌸Anika✧🌙';
-global.botname = '🌸Anika✧🌙';
-global.textbot = 'Estoy aquí para servirte 💁‍♀️✨';
-global.listo = '¿Qué más puedo hacer por ti? 😊💖';
-global.namechannel = '🌸Anika✧🌙';
-global.baileys = '@whiskeysockets/baileys';
-
-//*─────────────────────────────────────────────────────────────────*
-
-global.catalogo = fs.readFileSync('./storage/img/catalogo.png');
-global.siskedurl = fs.readFileSync('./storage/img/siskedurl.jpg');
-
-//*─────────────────────────────────────────────────────────────────*
-
-global.group = 'https://chat.whatsapp.com/KGdftYYFrbVBzTpZE73LBI';
-global.canal = 'https://whatsapp.com/channel/0029Vb3uTsb90x2rvI6D3G3b';
-
-//*─────────────────────────────────────────────────────────────────*
-
-global.estilo = { 
-  key: { 
-    fromMe: false, 
-    participant: '0@s.whatsapp.net', 
-    ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) 
-  }, 
-  message: { 
-    orderMessage: { 
-      itemCount: -999999, 
-      status: 1, 
-      surface: 'CATALOG', 
-      message: 'Anika Bot', 
-      orderTitle: 'Menu',
-      sellerJid: '0@s.whatsapp.net' 
+const config = {
+  owner: [
+    ['', 'Neykoor', true],
+    ['', '', true],
+  ],
+  packname: '🌸Anika✧🌙',
+  author: '🌸Anika✧🌙',
+  wait: '🌸Anika✧🌙',
+  botname: '🌸Anika✧🌙',
+  textbot: 'Estoy aquí para servirte 💁‍♀️✨',
+  listo: '¿Qué más puedo hacer por ti? 😊💖',
+  namechannel: '🌸Anika✧🌙',
+  baileys: '@whiskeysockets/baileys',
+  catalogo: fs.readFileSync('./storage/img/catalogo.png', { encoding: 'base64' }),
+  siskedurl: fs.readFileSync('./storage/img/siskedurl.jpg', { encoding: 'base64' }),
+  group: 'https://chat.whatsapp.com/KGdftYYFrbVBzTpZE73LBI',
+  canal: 'https://whatsapp.com/channel/0029Vb3uTsb90x2rvI6D3G3b',
+  estilo: { 
+    key: { 
+      fromMe: false, 
+      participant: '0@s.whatsapp.net', 
+      ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) 
+    }, 
+    message: { 
+      orderMessage: { 
+        itemCount: -999999, 
+        status: 1, 
+        surface: 'CATALOG', 
+        message: 'Anika Bot', 
+        orderTitle: 'Menu',
+        sellerJid: '0@s.whatsapp.net' 
+      } 
     } 
-  } 
+  },
+  multiplier: 69,
+  maxwarn: '2',
 };
 
-//*─────────────────────────────────────────────────────────────────*
-
-global.cheerio = cheerio;
-global.fs = fs;
-global.fetch = fetch;
-global.axios = axios;
-global.moment = moment;
-
-//*─────────────────────────────────────────────────────────────────*
-
-global.multiplier = 69;
-global.maxwarn = '2'; // máxima advertencias
-
-//*─────────────────────────────────────────────────────────────────*
-
-let file = fileURLToPath(import.meta.url);
-watchFile(file, () => {
-  unwatchFile(file);
-  console.log(chalk.redBright("Update 'config.js'"));
-  import(`${file}?update=${Date.now()}`);
-});
+export default config;
