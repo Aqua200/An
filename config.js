9c398542a8fa52465b07c1805484022c17b4efd1
Aqua200 <chinquepapa@gmail.com>
2025-01-31 14:52:49 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-import { watchFile, unwatchFile } from 'fs';
-import chalk from 'chalk';
-import { fileURLToPath } from 'url';
-import fs from 'fs';
-import cheerio from 'cheerio';
-import fetch from 'node-fetch';
-import axios from 'axios';
-import moment from 'moment-timezone';
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone' 
 
 //*─────────────────────────────────────────────────────────────────*
 
