833c471e8b3cf52fca528edac4e93f0a0de5d1ac
Aqua200 <chinquepapa@gmail.com>
2025-01-31 15:40:13 -0800

    Update borrar_carpetas.sh

--- lib/borrar_carpetas.sh
+++ lib/borrar_carpetas.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Lista de carpetas a verificar
-carpetas=("folder1" "folder2" "folder3")
+     # Lista de carpetas a verificar
+     carpetas=("folder1" "folder2" "folder3")
 
-for carpeta in "${carpetas[@]}"; do
-  if [ ! -d "$carpeta" ]; then
-    echo "La carpeta $carpeta no existe. Borrando..."
-    rm -rf "$carpeta"
-  else
-    echo "La carpeta $carpeta existe."
-  fi
-done
+     for carpeta in "${carpetas[@]}"; do
+       if [ -d "$carpeta" ]; then
+         echo "La carpeta $carpeta existe. Borrando..."
+         rm -rf "$carpeta"
+       else
+         echo "La carpeta $carpeta no existe."
+       fi
+     done
